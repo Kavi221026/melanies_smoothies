7e752cc157e61d63bdff680529eec373821250bd
Kavi221026 <127374712+Kavi221026@users.noreply.github.com>
2025-06-19 10:57:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,12 +35,11 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     ingredients_string = ''
     
-    st.subheader('Your Order Summary:')
-    st.write(f'Name: {name_on_order}')
-    st.write(f'Ingredients: {", ".join(ingredients_list)}')
-    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + ' Nutrition Information') 
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
